0fb5339f36695119e358f441b4cb47832b91d60b
Dieudonne Mukunzi <dmukunzi0@gmail.com>
2017-03-15 11:54:00 +0200

    Added columns to the pdf

--- app/pdfs/student_pdf.rb
+++ app/pdfs/student_pdf.rb
@@ -8,32 +8,46 @@ class StudentPdf < Prawn::Document
 		# 	fill_circle [0, 0], 30
 
 			@student = student
+			title
 			student_info
-			profile
+			profile_title
+			profile_content
 			experience
 			social
 		# end
 	end
 
-	def student_info
-		text_box "RESUME", style: :italic, align: :center, size: 30, :at => [0, 200]
-		text_box "#{@student.first_name} #{@student.last_name}", size: 30, style: :bold, align: :center
-		text_box "Email: #{@student.email}", size: 20, style: :italic, align: :center
-		text_box "Tel: #{@student.phone_number}", size: 16, style: :italic, align: :center
+	def title
+		text "RESUME", style: :italic, align: :center, size: 30
+		move_down 30
+	end
 
-		image open(@student.photo_url), :width => 100, :at => [300, 700]
+	def student_info
+		text "#{@student.first_name} #{@student.last_name}", size: 30, style: :bold
+		text "Email: #{@student.email}", size: 20, style: :italic
+		text "Tel: #{@student.phone_number}", size: 16, style: :italic
+
+		image open(@student.photo_url), :width => 100, :at => [400, 650]
 	end
 
 
-	def profile
+	def profile_title
 		move_down 30
 		fill_color "00bbdd"
-		font_size(18) do
+		font_color(18) do
 			text "PROFILE", style: :bold
 		end
 		stroke do
+			stroke_color "00bbdd"
+			move_down 10
 			horizontal_line 0, 550
+			move_down 10
 		end
+	end
+	
+	def profile_content
+		text "Date available to work: **/**/**", style: :bold
+		text "I am affable and forward thinking with a passion for life and the people that live it. (CHANGE!)"
 	end
 
 	def experience
